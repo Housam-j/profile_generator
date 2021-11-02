972f82aa4a2638ff341c1f8ed328521a9f60100f
Housam-j <housam-j@hotmail.com>
2021-11-02 23:25:59 +0000

    done

--- survey.js
+++ survey.js
@@ -9,8 +9,11 @@ rl.question('What\'s an activity you like doing?', (answer1) => {
   rl.question('What do you listen to while doing that?', (answer2) => {
     rl.question('What do you think this task for? ', (answer3) => {
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer4) => {
-    console.log(`Which sport is your absolute favourite?: ${answer1}` + `${answer2}`+`${answer3}`+`${answer4}`);
+        console.log(`Which sport is your absolute favourite?: ${answer1}\n` + `What do you listen to while doing that?${answer2}\n` + `What do you think this task for?${answer3}\n` + `Which meal is your favourite (eg: dinner, brunch, etc.)${answer4}`);
      
-    rl.close();
+        rl.close();
     
-})})})});
+      });
+    });
+  });
+});
