44f96920f5ce0e917520be031ce47a36d1110d5d
Housam-j <housam-j@hotmail.com>
2021-11-02 23:40:24 +0000

    done

--- survey.js
+++ survey.js
@@ -1,5 +1,4 @@
 const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -9,7 +8,7 @@ rl.question('What\'s an activity you like doing?', (answer1) => {
   rl.question('What do you listen to while doing that?', (answer2) => {
     rl.question('What do you think this task for? ', (answer3) => {
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer4) => {
-        console.log(`Which sport is your absolute favourite?: ${answer1}\n` + `What do you listen to while doing that?${answer2}\n` + `What do you think this task for?${answer3}\n` + `Which meal is your favourite (eg: dinner, brunch, etc.)${answer4}`);
+        console.log(`So, you like doing ${answer1}\n` + `and like listening to ${answer2} while doing that \n` + `and you think tjhis survey is for ${answer3}\n` + `and your favorite meal is${answer4}`);
      
         rl.close();
     
